0276888074e0a3def8e0cdf6002b50d0945250e1
Kristen Papke <klpapke215@gmail.com>
2018-07-14 22:24:47 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -10,7 +10,6 @@ end
 ["Tim", "Tom", "Jim"]
 hello_t(["Tim", "Tom", "Jim"]) do |name|
   if name.start_with?("T")
-    "Hi, #{name}"
+    puts "Hi, #{name}"
   end
-  return ("Tim", "Tom", "Jim")
 end
